27a03c0d88cd8c3ac926c5d7f51d50c4771ecea7
IgorSalinas <igor_salinas@outlook.com>
2022-11-03 22:00:41 -0300

    [feat]protochain v0.1 validation class

--- __tests__/block.test.ts
+++ __tests__/block.test.ts
@@ -12,13 +12,13 @@ describe("Block tests", () => {
     test('Should be valid', () => {
         const block = new Block(1, genesis.hash, 'block ex.');
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toEqual(true);
+        expect(valid.success).toBeTruthy();
     })
     
     test('Should be invalid (previousHash)', () => {
         const block = new Block(1, '', 'block ex.');
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toBeFalsy();
+        expect(valid.success).toBeFalsy();
     })
 
     test('Should be invalid (timestamp)', () => {
@@ -26,26 +26,26 @@ describe("Block tests", () => {
         block.timestamp = -1;
         block.hash = block.getHash();
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toBeFalsy();
+        expect(valid.success).toBeFalsy();
     })
 
     test('Should be invalid (hash)', () => {
         const block = new Block(1, genesis.hash, 'block ex.');
         block.hash = '';
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toBeFalsy();
+        expect(valid.success).toBeFalsy();
     })
 
     test('Should be invalid (data)', () => {
         const block = new Block(1, genesis.hash, '');
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toBeFalsy();
+        expect(valid.success).toBeFalsy();
     })
 
     test('Should be invalid (index)', () => {
         const block = new Block(-1, genesis.hash, 'block ex.');
         const valid = block.isValid(genesis.hash, genesis.index);
-        expect(valid).toBeFalsy();
+        expect(valid.success).toBeFalsy();
     })
 
 })
---
+++ src/lib/validation.ts
@@ -0,0 +1,18 @@
+
+/**
+ * Validation Class
+*/
+export default class Validation {
+    success: boolean;
+    message: string;
+
+    /**
+     * Create a new validation object 
+     * @param success If validation was successful
+     * @param message The validation message, if validation failed
+     */
+    constructor(success: boolean = true, message: string = ''){
+        this.success = success;
+        this.message = message
+    }
+}
